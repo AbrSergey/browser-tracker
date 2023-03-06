e0eee191c08f3de2ef737bd0768b79beb29f6c39
Sergey Abramov <sabramov@insquad.com>
2023-03-06 22:41:30 +0600

    chore: update private names

--- tracker.ts
+++ tracker.ts
@@ -7,17 +7,17 @@ interface ITracker {
 }
 
 class Tracker implements ITracker {
-  private buffer: Array<object>;
-  private timer_ids: Array<NodeJS.Timeout>;
-  private last_send_time: number;
-  private send: Function;
+  private _buffer: Array<object>;
+  private _timer_ids: Array<NodeJS.Timeout>;
+  private _last_send_time: number;
+  private _send: Function;
 
   constructor() {
-    this.buffer = [];
-    this.timer_ids = [];
-    this.last_send_time = 0;
+    this._buffer = [];
+    this._timer_ids = [];
+    this._last_send_time = 0;
 
-    this.send = function (arr: Array<object>) {
+    this._send = function (arr: Array<object>) {
       return fetch(CONFIG_TRACK_URL, {
         method: 'POST',
         headers: {
@@ -27,45 +27,45 @@ class Tracker implements ITracker {
         body: JSON.stringify(arr),
       }).catch((_err) => {
         setTimeout(() => {
-          this.buffer.push(...arr);
+          this._buffer.push(...arr);
         }, CONFIG_TIMEOUT);
       });
     };
 
     window.onbeforeunload = () => {
-      if (this.buffer.length) {
+      if (this._buffer.length) {
         console.info(
-          `send by onbeforeunload buffer.length = ${this.buffer.length}`,
+          `send by onbeforeunload buffer.length = ${this._buffer.length}`,
         );
-        this.send(this.buffer);
+        this._send(this._buffer);
       }
     };
   }
 
-  private captureEvent() {
-    this.send([...this.buffer]);
-    this.buffer.length = 0;
-    this.last_send_time = new Date().getTime();
-    this.timer_ids.forEach((i) => clearTimeout(i));
-    this.timer_ids.length = 0;
+  private _captureEvent() {
+    this._send([...this._buffer]);
+    this._buffer.length = 0;
+    this._last_send_time = new Date().getTime();
+    this._timer_ids.forEach((i) => clearTimeout(i));
+    this._timer_ids.length = 0;
   }
 
-  private onPush() {
+  private _onPush() {
     let now = new Date().getTime();
 
-    if (now - this.last_send_time > CONFIG_TIMEOUT) {
-      console.info(`send immediately buffer.length = ${this.buffer.length}`);
-      this.captureEvent();
-    } else if (this.buffer.length >= CONFIG_MAX_BUF_LENGTH) {
-      console.info(`send by length buffer.length = ${this.buffer.length}`);
-      this.captureEvent();
+    if (now - this._last_send_time > CONFIG_TIMEOUT) {
+      console.info(`send immediately buffer.length = ${this._buffer.length}`);
+      this._captureEvent();
+    } else if (this._buffer.length >= CONFIG_MAX_BUF_LENGTH) {
+      console.info(`send by length buffer.length = ${this._buffer.length}`);
+      this._captureEvent();
     } else {
       const timerId = setTimeout(() => {
-        console.info(`send by time length = ${this.buffer.length}`);
-        this.captureEvent();
+        console.info(`send by time length = ${this._buffer.length}`);
+        this._captureEvent();
       }, CONFIG_TIMEOUT);
 
-      this.timer_ids.push(timerId);
+      this._timer_ids.push(timerId);
     }
   }
 
@@ -74,7 +74,7 @@ class Tracker implements ITracker {
     const title = document.title;
     const ts = new Date().getTime();
 
-    this.buffer.push({
+    this._buffer.push({
       event,
       tags,
       url,
@@ -82,7 +82,7 @@ class Tracker implements ITracker {
       ts,
     });
 
-    this.onPush();
+    this._onPush();
   }
 }
 
